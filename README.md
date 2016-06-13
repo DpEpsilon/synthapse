0829dff92dd3ac4641087510fe1df19408779c2c
Daniel Phillips <dyproepsilon@gmail.com>
2016-06-13 11:00:44 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+Synthapse
+=========
+
+This is a hacky little toy synthesiser I wrote when I wanted to make my major
+work for my high school leaving certificate in music sound like a cheap
+synthesiser from the 1960s. I'm yet to clean it up.
+
+Written in python 2. Works great with pypy, and you'll probably be needing it.
+
+The sample sequence in synth.py is one half of the melody of that ridiculous
+composition of mine. There were two parts, this one was square-waveish and
+lower, the other was sawtooth-waveish and higher.
---
+++ sound_writer.py
@@ -0,0 +1,36 @@
+import wave
+
+def sopen(filename):
+    f = wave.open(filename, 'w')
+    f.setnchannels(2)
+    f.setsampwidth(2)
+    f.setframerate(44100)
+    return f
+
+def swrite(f, samp_l, samp_r):
+    samp = bytearray(2*2)
+    samp_l = int((samp_l + 1.0)/2 * 65535) - 2**15
+    samp_r = int((samp_r + 1.0)/2 * 65535) - 2**15
+    samp[0] = samp_l & 255
+    samp[1] = (samp_l & 255 * 256) / 256
+    samp[2] = samp_r & 255
+    samp[3] = (samp_r & 255 * 256) / 256
+    f.writeframes(buffer(samp))
+
+def swrite_long(f, samp_l, samp_r):
+    assert(len(samp_l) == len(samp_r))
+    l = len(samp_l)
+    samp = bytearray(2*2*l)
+    for i in xrange(l):
+        sl = int((samp_l[i] + 1.0)/2 * 65535) - 2**15
+        sr = int((samp_r[i] + 1.0)/2 * 65535) - 2**15
+        samp[i*4 + 0] = sl & 255
+        samp[i*4 + 1] = (sl & 255 * 256) / 256
+        samp[i*4 + 2] = sr & 255
+        samp[i*4 + 3] = (sr & 255 * 256) / 256
+    f.writeframes(buffer(samp))
+
+def sclose(f):
+    f.close()
+    
+    
